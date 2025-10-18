4b956398ce1cb37aded216261d0bd9a1a6bb884e
MB <oppoa5s.mbcell@gmail.com>
2025-10-18 18:43:08 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Script sederhana untuk form
+document.getElementById('contact-form').addEventListener('submit', function(e){
+    e.preventDefault();
+    alert("Terima kasih! Pesan Anda telah dikirim.");
+    this.reset();
+});
